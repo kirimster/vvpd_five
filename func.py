0f8d015ba3f13ecd1e3ed9bcc32fcfbacb9b81bf
Alexander <alexanderbatyuta@yandex.ru>
2024-12-16 23:37:44 +0700

    Создание меню для вызова функций

--- func.py
+++ func.py
@@ -1,6 +1,15 @@
 import math
 
 
+def menu():
+    print("\nВыберите ряд для вычисления:"
+    "\n1. Ряд для косинуса (cos(x))"
+    "\n2. Ряд для арктангенса (arctan(x))"
+    "\n3. Биномиальное разложение (1 + x)^m"
+    "\n4. Выход"
+          )
+
+
 def cos_series(x, terms=10):
     result = 0
     for n in range(terms):
@@ -13,3 +22,31 @@ def arctan_series(x, terms=10):
     for n in range(terms):
         result += ((-1) ** n) * (x ** (2 * n + 1)) / (2 * n + 1)
     return result
+
+
+def main():
+    while True:
+        menu()
+        choice = input("Выберите опцию (1-4): ")
+
+        if choice == "1":
+            x = float(input("Введите значение x для cos(x): "))
+            terms = int(input("Введите количество членов для приближенного вычисления: "))
+            print(f"cos({x}) ≈ {cos_series(x, terms)}")
+
+        elif choice == "2":
+            x = float(input("Введите значение x для arctan(x): "))
+            terms = int(input("Введите количество членов для приближенного вычисления: "))
+            print(f"arctan({x}) ≈ {arctan_series(x, terms)}")
+
+
+        elif choice == "4":
+            print("Выход из программы.")
+            break
+        else:
+            print("Некорректный ввод. Пожалуйста, выберите правильный вариант.")
+
+
+
+if __name__ == "__main__":
+    main()
